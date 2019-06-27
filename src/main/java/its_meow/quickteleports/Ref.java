278ec84d04d20748c33889f6a28a29d3fc18e073
itsmeow <mods.itsmeow@gmail.com>
2019-06-26 22:19:07 -0400

    Update buildscript

--- src/main/java/its_meow/quickteleports/Ref.java
+++ src/main/java/its_meow/quickteleports/Ref.java
@@ -3,6 +3,6 @@ package its_meow.quickteleports;
 public class Ref {
 	public static final String MOD_ID = "quickteleports";
 	public static final String NAME = "Quick Teleports";
-	public static final String VERSION = "1.1.0";
-	public static final String acceptedMCV = "[1.12,1.13)";
+	public static final String VERSION = "@VERSION@";
+	public static final String acceptedMCV = "[1.12,1.12.2]";
 }
